63ff97a738ed315bdcef824d32d63bdc814aab23
N.Mice <n.mice@mail.ru>
2020-10-08 00:44:31 +0300

    hw01-gradle - added gradle module

---
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -0,0 +1,13 @@
+package ru.otus;
+
+import com.google.common.math.IntMath;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+
+
+        /*int x = 1;
+        int y = IntMath.ceilingPowerOfTwo(1);
+        System.out.println(y);*/
+    }
+}
