2dbcfff6bc83e947e5c2a3a0c2cf59bb728cf4f9
N.Mice <n.mice@mail.ru>
2020-10-15 19:33:36 +0300

    fix structure

--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -1,13 +1,12 @@
 package ru.otus;
 
-import com.google.common.math.IntMath;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
 
 public class HelloOtus {
     public static void main(String[] args) {
-
-
-        /*int x = 1;
-        int y = IntMath.ceilingPowerOfTwo(1);
-        System.out.println(y);*/
+        Map<Integer, String> numByNameMap = ImmutableMap.of(1,"One");
+        System.out.println(numByNameMap);
     }
 }
