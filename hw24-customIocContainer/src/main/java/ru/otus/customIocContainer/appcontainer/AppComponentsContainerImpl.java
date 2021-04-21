02436103c2bd9021f0331308e3ad2e44b0a657df
N.Mice <n.mice@mail.ru>
2021-04-21 09:35:33 +0300

    hw24 - added solve

--- hw24-customIocContainer/src/main/java/ru/otus/customIocContainer/appcontainer/AppComponentsContainerImpl.java
+++ hw24-customIocContainer/src/main/java/ru/otus/customIocContainer/appcontainer/AppComponentsContainerImpl.java
@@ -11,13 +11,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/**
- * TODO имплементация
- */
 public class AppComponentsContainerImpl implements AppComponentsContainer {
-    /**
-     * TODO сохранить 2 коллекции
-     */
+
     private static final Logger logger = LoggerFactory.getLogger(AppComponentsContainerImpl.class);
     private final List<Object> appComponents = new ArrayList<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
@@ -26,28 +21,15 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         processConfig(initialConfigClass);
     }
 
-    private void processConfig(Class<?> configClass) {
-        checkConfigClass(configClass);
-        /**
-         * TODO Просканировать папки на предмет реализаций
-         * Найти реализацию - класс с аннотацией AppConfig
-         * Создать экземпляр
-         * Сохранить в мапу и лист
-         */
-        // You code here...
-        final Object config = getConfigClassInstance(configClass);
-
-        List<Method> beansMethods = Stream.of(configClass.getDeclaredMethods())
-                .filter(method -> method.isAnnotationPresent(AppComponent.class))
-                .sorted(Comparator.comparingInt(
-                        method -> method.getAnnotation(AppComponent.class).order()))
-                .collect(Collectors.toList());
+    @Override
+    public <C> C getAppComponent(Class<C> componentClass) {
+        return (C) getBeanByClassName(componentClass);
     }
 
-    private void checkConfigClass(Class<?> configClass) {
-        if (!configClass.isAnnotationPresent(AppComponentsContainerConfig.class)) {
-            throw new IllegalArgumentException(String.format("Given class is not config %s", configClass.getName()));
-        }
+    @Override
+    public <C> C getAppComponent(String componentName) {
+        // Получаем бин из мапы по ключу - имени класса
+        return (C) appComponentsByName.get(componentName);
     }
 
     private Object getConfigClassInstance(Class<?> configClass) {
@@ -59,30 +41,54 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         }
     }
 
-    @Override
-    public <C> C getAppComponent(Class<C> componentClass) {
-        C component = null;
-        if (componentClass.isInterface()) {
-            /**
-             * TODO Просканировать папки на предмет реализаций
-             * Найти реализацию - класс с аннотацией AppConfig
-             * Создать экземпляр
-             * Сохранить в мапу и лист
-             */
-            Class<C> componentClassImpl;
-            //component = componentClassImpl.getDeclaredConstructor().newInstance();
-
-        } else {
-            //component = componentClass.getDeclaredConstructor().newInstance();
+    private void processConfig(Class<?> configClass) {
+        checkConfigClass(configClass);
+        // Получить экземпляр конфига
+        Object config = getConfigClassInstance(configClass);
+        // Получить список всех методов, аннотированных AppComponent, упорядочить по order
+        List<Method> beansMethods = Stream.of(configClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(AppComponent.class))
+                .sorted(Comparator.comparingInt(method -> method.getAnnotation(AppComponent.class).order()))
+                .collect(Collectors.toList());
+        // Пройти по списку методов
+        for (Method method : beansMethods) {
+            // Для каждого метода получить массив аргументов
+            var args = getArgsArrayForMethod(method);
+            try {
+                // Получить инстанс бина, вызвав метод с аргументами у инстанса конфига
+                Object resultBean = method.invoke(config, args);
+                // Добавить бин в коллекцию
+                appComponents.add(resultBean);
+                // Получить имя бина для мапы из аннотации
+                String beanName = method.getDeclaredAnnotation(AppComponent.class).name();
+                // Добавить бин в мапу
+                appComponentsByName.put(beanName, resultBean);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
-        return component;
     }
 
-    @Override
-    public <C> C getAppComponent(String componentName) {
-        /**
-         * TODO код здесь
-         */
-        return null;
+    private Object[] getArgsArrayForMethod(Method method) {
+        // Получить массив типов параметров метода
+        Class<?>[] parameterTypes = method.getParameterTypes();
+        // Получить массив параметров из мапы по типам
+        return Stream.of(parameterTypes)
+                .map(this::getBeanByClassName)
+                .toArray();
+    }
+
+    private Object getBeanByClassName(Class<?> componentClass) {
+        // Сверяем классы бинов из коллекции с запрашиваемым классом
+        return appComponents.stream()
+                .filter(component -> componentClass.isAssignableFrom(component.getClass()))
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("Bean " + componentClass.getName() + "not found"));
+    }
+
+    private void checkConfigClass(Class<?> configClass) {
+        if (!configClass.isAnnotationPresent(AppComponentsContainerConfig.class)) {
+            throw new IllegalArgumentException(String.format("Given class is not config %s", configClass.getName()));
+        }
     }
 }
